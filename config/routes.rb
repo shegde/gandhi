3a0a0971224412f61f4d1e514672fe2a9ac2ac4f
Shailesh <hegde.shailesh@gmail.com>
2012-01-02 16:12:22 -0500

    more pg stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Gandhi::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/about"
-
+  match 'home' => 'pages#home'
+  match 'about' => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +51,7 @@ Gandhi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
