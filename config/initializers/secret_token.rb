c2977a516ec91e2b755022cb3f5a09dd041c6eda
Shailesh <hegde.shailesh@gmail.com>
2012-01-02 16:03:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gandhi::Application.config.secret_token = '3b2faac3a2638a06d1428df65c55f5ed254e37db38dbd037c5dbf55e6423d5e44c360f2ebda47dd3527eb8fb23d6e19dde3ff51be3d9a4bd80faf2499522d7e1'
