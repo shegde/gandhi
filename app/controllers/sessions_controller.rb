0959578ada7780c836a1fac27ab52d71561e98f8
Shailesh <hegde.shailesh@gmail.com>
2012-01-02 17:03:20 -0500

    more omniauth stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
 class SessionsController < ApplicationController
+  
   def create
-    render :text => request.env['omniauth.auth'].inspect
+    # render :text => request.env['omniauth.auth'].inspect
+    auth = request.env['omniauth.auth']
+    unless @auth = Authorization.find_from_hash(auth)
+      # Create a new user or add an auth to existing user, depending on
+      # whether there is already a user signed in.
+      @auth = Authorization.create_from_hash(auth, current_user)
+    end
+    # Log the authorizing user in.
+    self.current_user = @auth.user
+    
+    # render :text => "Welcome, #{current_user.name}."
+    flash.now[:success] = "Welcome, #{current_user.name}."
+    redirect_to root_path
+  end
+  
+  def failure
+    # render :text => "cannot connect to server! Please try again."
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
